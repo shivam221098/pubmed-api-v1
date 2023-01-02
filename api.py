17dbb1e446bad2d8c931a44ad882b708d70b2616
SHIVAM SINGH <shivam221098@gmail.com>
2023-01-02 13:02:35 +0530

    Added params

--- api.py
+++ api.py
@@ -1,6 +1,42 @@
 from typing import Dict, List
 from requests import Session
 from xmltodict import parse
+from datetime import datetime
+
+
+class ResultSet:
+    def __init__(self):
+        pass
+
+
+class Params:
+    def __init__(
+            self,
+            term: str,
+            ret_max: int = 10_000,
+            min_date=datetime(year=1500, month=1, day=1),
+            max_date=datetime.utcnow()
+    ):
+        self.__term = term
+        self.__ret_max = ret_max
+        self.__min_date = min_date
+        self.__max_date = max_date
+
+    @property
+    def min_date(self):
+        return self.__min_date
+
+    @min_date.setter
+    def min_date(self, new_min):
+        self.__min_date += new_min
+
+    @property
+    def max_date(self):
+        return self.__max_date
+
+    @max_date.setter
+    def max_date(self, new_max):
+        self.__max_date += new_max
 
 
 class API(Session):
