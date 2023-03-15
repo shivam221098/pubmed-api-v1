15c160867d8c99d839ad0af73522ec13fd25cb58
shivam <shivam221098@gmail.com>
2023-03-15 08:12:22 +0530

    Changed search string

--- pubmed.py
+++ pubmed.py
@@ -38,7 +38,7 @@ class PubMedAPI:
 if __name__ == '__main__':
     start = time.time()
     p = PubMedAPI()
-    e = p.extract('((("NASH"[title/abstract] AND "liver"[title/abstract]) OR ("NASH"[title/abstract] AND "hepatitis"[title/abstract]) OR ("NASH"[title/abstract] AND "steatosis"[title/abstract]) OR ("NASH"[title/abstract] AND "cirrhosis"[title/abstract]) OR ("NASH"[title/abstract] AND "obesity"[title/abstract]) OR ("NASH"[title/abstract] AND "diabetes"[title/abstract]) OR "NAFLD"[title/abstract] OR "NAFL"[title/abstract] OR "non-alcoholic steatohepatitis"[title/abstract]) OR "non alcoholic steatohepatitis"[title/abstract] OR "nonalcoholic steatohepatitis"[title/abstract] OR "non-alcoholic fatty liver"[title/abstract] OR "non alcoholic fatty liver"[title/abstract] OR "nonalcoholic fatty liver"[title/abstract] OR "NAFLD"[MeSH]) NOT ("mice"[title/abstract] OR "murine"[title/abstract] OR "mouse"[title/abstract] OR "rat"[title/abstract] OR "hepatitis C"[title/abstract] OR "hepatitis B"[title/abstract] OR "HCV"[title/abstract] OR "HBV"[title/abstract] OR "HIV"[title/abstract] OR "macrophage"[title/abstract] OR "microphage"[title/abstract] OR "regulator"[title/abstract] OR "phase 1"[title/abstract] OR "Phase I"[title/abstract] OR "animal"[title/abstract])')
+    e = p.extract('"parkinson\'s disease"')
     # print(e.pmids)
     print(f"PMID Count: {e.record_count}")
     print(f"Time Taken: {time.time() - start}")
