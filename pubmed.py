d152272b2bbd1fbead02ac9b3200ee02424375bc
shivam <shivam221098@gmail.com>
2023-03-15 08:59:34 +0530

    Added TestCases and removed unused code

--- pubmed.py
+++ pubmed.py
@@ -1,34 +1,33 @@
 import time
 from api import API, Params
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
 
 class PubMedAPI:
     def __init__(self):
         self.__api = API()
-        self.__results = []
 
     @property
     def api(self):
         return self.__api
 
     def extract(self, term):
-        param = Params(term)
+        param = Params(term)  # setting up params
         return self.get_pmids(param)
 
-    def get_pmids(self, param: Params, start=1):
+    def get_pmids(self, param: Params, start_=1):
         result = self.api.get_response(param)
         if result.record_count <= len(result):
             return result
 
+        # take out maximum PMID
         max_pmid = max(result.pmids)
 
-        param.uid_start = start
-        param.uid_end = (start + max_pmid) // 2
-        left = self.get_pmids(param, start)
+        # divide the PMID into two halves and run API for each half
+        param.uid_start = start_
+        param.uid_end = (start_ + max_pmid) // 2
+        left = self.get_pmids(param, start_)
 
-        param.uid_start = (start + max_pmid) // 2
+        param.uid_start = (start_ + max_pmid) // 2
         param.uid_end = max_pmid
         right = self.get_pmids(param, param.uid_start)
 
---
+++ tests.py
@@ -0,0 +1,69 @@
+"""
+Tests are written on 15th March 2023.
+We have PMIDs counts taken on 7th March 2023 (from confluence)
+We are expecting PMIDs counts for each search string which ran on 7th March lesser than or equal to today's date.
+"""
+import unittest
+from pubmed import PubMedAPI
+
+
+class Tests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.pubmed_api = PubMedAPI()
+
+    def test_search_string_chicken_and_dentist_less_than_10k(self):
+        """
+        search_string: chicken AND dentist
+        expected_pmid_count: >= 8
+        :return: None
+        """
+        search_string = "chicken AND dentist"
+        result = self.pubmed_api.extract(search_string)
+        self.assertLessEqual(8, result.record_count)
+
+    def test_search_string_hair_pain_or_ring_pain_less_than_10k(self):
+        """
+        search_string: "hair pain" OR "ring pain"
+        expected_pmid_count: >= 10
+        :return: None
+        """
+        search_string = '"hair pain" OR "ring pain"'
+        result = self.pubmed_api.extract(search_string)
+        self.assertLessEqual(10, result.record_count)
+
+    def test_search_string_ankle_modelling_more_than_10k(self):
+        """
+        search_string: ankle modelling
+        expected_pmid_count: >= 10368
+        :return: None
+        """
+        search_string = 'ankle modelling'
+        result = self.pubmed_api.extract(search_string)
+        self.assertGreaterEqual(result.record_count, 10368)
+
+    def test_search_string_parkinson_disease_more_than_100k(self):
+        """
+        search_string: "parkinson\'s disease"
+        expected_pmid_count: >= 106613
+        :return: None
+        """
+        search_string = '"parkinson\'s disease"'
+        result = self.pubmed_api.extract(search_string)
+        self.assertGreaterEqual(result.record_count, 106613)
+
+    def test_search_string_human_immunodeficiency_virus_more_than_10k(self):
+        """
+        search_string: "human immunodeficiency virus (hiv)"
+        expected_pmid_count: >= 47164
+        :return: None
+        """
+        search_string = '"human immunodeficiency virus (hiv)"'
+        result = self.pubmed_api.extract(search_string)
+        self.assertGreaterEqual(result.record_count, 47164)
+
+    def tearDown(self) -> None:
+        self.pubmed_api = None
+
+
+if __name__ == '__main__':
+    unittest.main()
