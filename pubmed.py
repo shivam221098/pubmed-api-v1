c4525b5214c6583e2780371edb8e10d9cc298472
shivam <shivam221098@gmail.com>
2023-03-15 07:42:44 +0530

    Too generic names changed to more understandable names

--- pubmed.py
+++ pubmed.py
@@ -22,14 +22,14 @@ class PubMedAPI:
         if result.record_count <= len(result):
             return result
 
-        maxi = max(result.pmids)
+        max_pmid = max(result.pmids)
 
         param.uid_start = start
-        param.uid_end = (start + maxi) // 2
+        param.uid_end = (start + max_pmid) // 2
         left = self.get_pmids(param, start)
 
-        param.uid_start = (start + maxi) // 2
-        param.uid_end = maxi
+        param.uid_start = (start + max_pmid) // 2
+        param.uid_end = max_pmid
         right = self.get_pmids(param, param.uid_start)
 
         return left + right
@@ -38,6 +38,7 @@ class PubMedAPI:
 if __name__ == '__main__':
     start = time.time()
     p = PubMedAPI()
-    e = p.extract('"parkinson\'s disease"')
-    print(e.pmids, e.record_count, len(e.pmids))
+    e = p.extract('((("NASH"[title/abstract] AND "liver"[title/abstract]) OR ("NASH"[title/abstract] AND "hepatitis"[title/abstract]) OR ("NASH"[title/abstract] AND "steatosis"[title/abstract]) OR ("NASH"[title/abstract] AND "cirrhosis"[title/abstract]) OR ("NASH"[title/abstract] AND "obesity"[title/abstract]) OR ("NASH"[title/abstract] AND "diabetes"[title/abstract]) OR "NAFLD"[title/abstract] OR "NAFL"[title/abstract] OR "non-alcoholic steatohepatitis"[title/abstract]) OR "non alcoholic steatohepatitis"[title/abstract] OR "nonalcoholic steatohepatitis"[title/abstract] OR "non-alcoholic fatty liver"[title/abstract] OR "non alcoholic fatty liver"[title/abstract] OR "nonalcoholic fatty liver"[title/abstract] OR "NAFLD"[MeSH]) NOT ("mice"[title/abstract] OR "murine"[title/abstract] OR "mouse"[title/abstract] OR "rat"[title/abstract] OR "hepatitis C"[title/abstract] OR "hepatitis B"[title/abstract] OR "HCV"[title/abstract] OR "HBV"[title/abstract] OR "HIV"[title/abstract] OR "macrophage"[title/abstract] OR "microphage"[title/abstract] OR "regulator"[title/abstract] OR "phase 1"[title/abstract] OR "Phase I"[title/abstract] OR "animal"[title/abstract])')
+    print(e.pmids)
+    print(f"PMID Count: {e.record_count}")
     print(f"Time Taken: {time.time() - start}")
