a84e6b277be4b30d3b9c76fe1e3741d1a1db7424
shivam <shivam221098@gmail.com>
2023-03-28 13:06:24 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+
+setuptools.setup(
+    name="pubmed-api",
+    version="1.0.0",
+    author="Shivam",
+    author_email="shivam@visfo.health",
+    description="Runs PubMed search strings over pubmed API using a batch logic",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shivam221098/pubmed-api-v1.git",
+    packages=["pubmed-api"],
+    install_requires=[
+        "xmltodict",
+        "requests"
+    ]
+)
