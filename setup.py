c969c780f79c825d995d23aa87cb78459449584b
shivam <shivam221098@gmail.com>
2023-04-02 11:02:15 +0530

    Changed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setuptools.setup(
     name="pubmed_api",
-    version="1.1.1",
+    version="1.1.2",
     author="Shivam",
     author_email="shivam221098@gmail.com",
     description="Runs PubMed search strings over pubmed API using a batch logic",
